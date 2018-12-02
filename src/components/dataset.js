bf8e15d76180e6ff2ff0b9b7f03d48a62c565b28
Louis Weng <louis396@hotmail.com>
2018-12-02 23:14:39 +0800

    add photo

--- src/components/dataset.js
+++ src/components/dataset.js
@@ -4,13 +4,14 @@ const React = require('react')
 const { PureComponent } = React
 const { injectIntl, intlShape } = require('react-intl')
 const { bool } = require('prop-types')
-const { Toolbar } = require('./toolbar')
+const { Tabs, Alert, List, Checkbox } = require('antd')
+const TabPane = Tabs.TabPane
 
 //Todo data set module
 class DataSet extends PureComponent {
 
-  renderToolbar() {
-    return this.props.showToolbar && <Toolbar/>
+  callback = (key) => {
+    console.log(key)
   }
 
   handleAddNewPhotos = () => {
@@ -20,10 +21,50 @@ class DataSet extends PureComponent {
   }
 
   render() {
+
+    const data = [
+      {
+        title: '可乐中国',
+        desc: '330ml 可乐,660ml可乐, 330ml 美汁源',
+      },
+      {
+        title: '智能驾驶上海',
+        desc: '蔚来汽车, 小鹏汽车',
+      },
+      {
+        title: '智能驾驶北京',
+        desc: '特斯拉model3, tesla Model X',
+      },
+      {
+        title: '能量型饮料',
+        desc: 'Monstar, 魔抓',
+      },
+    ]
+
     return (
       <div className="about-view">
-        {this.renderToolbar()}
-        <figure className="app-icon"/>
+        <Tabs defaultActiveKey="1" onChange={()=>this.callback} style={{ textAlign: 'center' }}>
+          <TabPane tab="添加图片" key="1">
+            <div style={{ padding: '0px 16px 16px' }}>
+              <Alert message="请选择以下一种方式添加图片" />
+            </div>
+          </TabPane>
+          <TabPane tab="已有图片" key="2">
+            <div style={{ padding: '0px 16px 16px' }}>
+              <List
+                itemLayout="horizontal"
+                dataSource={data}
+                renderItem={item => (
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={<Checkbox/>}
+                      title={<a href="https://ant.design">{item.title}</a>}
+                      description={item.desc}/>
+                  </List.Item>
+              )}/>
+            </div>
+          </TabPane>
+        </Tabs>
         <div className="flex-row center">
           <div>
             <h5>添加图片</h5>
@@ -35,17 +76,6 @@ class DataSet extends PureComponent {
             </div>
           </div>
           <hr/>
-          <div>
-            <h5>选择已有图片</h5>
-            <div>
-              <input type="checkbox" id="scales" name="scales"/>
-              <label htmlFor="scales">cola</label>
-            </div>
-            <div>
-              <input type="checkbox" id="horns" name="horns"/>
-              <label htmlFor="horns">北美</label>
-            </div>
-          </div>
         </div>
       </div>
     )
