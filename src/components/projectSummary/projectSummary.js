8aee0336ddf5f72dd8be69683eb72b3652a0fee3
Louis Weng <louis396@hotmail.com>
2018-12-05 10:05:44 +0800

    task list

--- src/components/projectSummary/projectSummary.js
+++ src/components/projectSummary/projectSummary.js
@@ -4,6 +4,7 @@ const React = require('react')
 const { PureComponent } = React
 const { Row, Col, Card, Tabs } = require('antd')
 const { Summary } = require('./summary')
+const { TaskList } = require('./taskList')
 const TabPane = Tabs.TabPane
 
 class ProjectSummary extends PureComponent {
@@ -27,9 +28,9 @@ class ProjectSummary extends PureComponent {
                 <TabPane tab="项目概述" key="1"><Summary/></TabPane>
                 <TabPane tab="图片数据" key="2">Content of Tab Pane 2</TabPane>
                 <TabPane tab="项目参与者" key="3">Content of Tab Pane 3</TabPane>
-                <TabPane tab="任务列表" key="4">Content of Tab Pane 3</TabPane>
-                <TabPane tab="工作日志" key="5">Content of Tab Pane 3</TabPane>
-                <TabPane tab="数据导出" key="6">Content of Tab Pane 3</TabPane>
+                <TabPane tab="任务列表" key="4"><TaskList/></TabPane>
+                <TabPane tab="工作日志" key="5">Content of Tab Pane 5</TabPane>
+                <TabPane tab="数据导出" key="6">Content of Tab Pane 6</TabPane>
               </Tabs>
             </Card>
           </Col>
