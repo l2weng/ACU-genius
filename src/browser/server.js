1f5215827288144ec3c7a1dc9401ab219525a069
Louis Weng <louis396@hotmail.com>
2018-12-11 14:43:59 +0800

    Add socket and mdns

--- src/browser/server.js
+++ src/browser/server.js
@@ -2,11 +2,9 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = Number(process.env.PORT) || 8188
-const bonjour = require('bonjour')()
-let workers = {}
 
 app.get('/', async function (req, res) {
-  res.status(200).send({ workers: workers })
+  res.status(200).send({ workers: [] })
 })
 
 io.on('connection', function (socket) {
@@ -20,22 +18,5 @@ io.on('connection', function (socket) {
 })
 
 http.listen(port, function () {
-  let bonjourService = bonjour.publish({ name: 'LabelReal Server', type: 'http', port: port })
-  bonjourService.on('up', function () {
-    bonjour.find({ type: 'http' }, function (service) {
-      if (workers[service.host]) return
-      let expression = /((\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b))/
-      const regex = new RegExp(expression, 'g')
-      let addresses = service.addresses
-      const matchedAddress = addresses.filter(address=>regex.test(address))
-      // if (Number(service.port) !== port) return
-      workers[service.host] = { address: matchedAddress[0], port: service.port }
-    })
-  })
-  bonjourService.on('down', function () {
-    console.log(bonjourService)
-    if (!workers[bonjourService.host]) return
-    delete workers[bonjourService.host]
-  })
   console.log('listening on *:' + port)
 })
