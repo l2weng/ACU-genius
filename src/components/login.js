6257dc628a5caf43c26cb8080e6d68403c6cb259
Louis Weng <louis396@hotmail.com>
2018-12-11 17:15:55 +0800

    add register form

--- src/components/login.js
+++ src/components/login.js
@@ -13,6 +13,7 @@ const {
 const FormItem = Form.Item
 
 class LoginForm extends Component {
+
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.form.validateFields((err, values) => {
@@ -31,27 +32,23 @@ class LoginForm extends Component {
             rules: [{ required: true, message: 'Please input your username!' }],
           })(
             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          )}
+            )}
         </FormItem>
         <FormItem>
           {getFieldDecorator('password', {
             rules: [{ required: true, message: 'Please input your Password!' }],
           })(
             <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          )}
+            )}
         </FormItem>
         <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
+
+          <Checkbox>Remember me</Checkbox>
+
           <a className="login-form-forgot" href="">Forgot password</a>
           <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
+              Log in
           </Button>
-          Or <a href="">register now!</a>
         </FormItem>
       </Form>
     )
@@ -65,13 +62,32 @@ class Login extends PureComponent {
     return this.props.showToolbar && <Toolbar/>
   }
 
+  state = { visible: false };
+
+  needRegister = () => {
+    this.setState({
+      visible: true,
+    })
+  };
+  needLogin = () => {
+    this.setState({
+      visible: false,
+    })
+  };
+
   render() {
+    const { visible } = this.state
     const WrappedNormalLoginForm = Form.create()(LoginForm)
     return (
       <div className="login-view">
         {this.renderToolbar()}
         <div className="flex-row center">
-          <WrappedNormalLoginForm/>
+          {!visible ? <WrappedNormalLoginForm/> : <div>register</div>}
+          {!visible ? <FormItem>
+            <a onClick={this.needRegister}>register now!</a>
+          </FormItem> : <FormItem>
+            <a onClick={this.needLogin}>back!</a>
+          </FormItem>}
         </div>
       </div>
     )
