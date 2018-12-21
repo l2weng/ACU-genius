e17d64ae70f811e05184a140c2a145e9cf58edd1
Louis Weng <louis396@hotmail.com>
2018-12-21 14:55:03 +0800

    unable asar

--- src/browser/labelReal.js
+++ src/browser/labelReal.js
@@ -90,19 +90,30 @@ class LabelReal extends EventEmitter {
   }
 
   open(file) {
+    // if (!file) {
+    //   if (this.win) return this.win.show(), this
+    //
+    //   if (this.state.recent.length > 0) {
+    //     return this.showRecent()
+    //   } else {
+    //     for (let recent of this.state.recent) {
+    //       if (!exists(recent)) continue
+    //       file = recent
+    //       break
+    //     }
+    //     if (!file) return this.showGuideline()
+    //   }
+    // }
     if (!file) {
       if (this.win) return this.win.show(), this
 
-      if (this.state.recent.length > 0) {
-        return this.showRecent()
-      } else {
-        for (let recent of this.state.recent) {
-          if (!exists(recent)) continue
-          file = recent
-          break
-        }
-        if (!file) return this.showGuideline()
+      for (let recent of this.state.recent) {
+        if (!exists(recent)) continue
+        file = recent
+        break
       }
+
+      return this.showWizard()
     }
 
     try {
