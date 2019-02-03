5e9cd08e10c0e4e5b93a489916af795c322598b0
Louis Weng <louis396@hotmail.com>
2019-02-03 12:46:22 +0800

    team contacts

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -30,23 +30,7 @@ class Contacts extends PureComponent {
             <Tabs style={{ textAlign: 'left' }}
               defaultActiveKey="1"
               tabPosition="left">
-              <TabPane tab="全部联系人" key="1">
-                <Card
-                  title="全部联系人"
-                  extra={<a href="#">添加联系人</a>}
-                  style={{ width: '95%' }}>
-                  <Avatar style={{ backgroundColor: '#4e72ab' }} icon="user" />
-                  <Avatar style={{ backgroundColor: '#c32964' }}>Terry</Avatar>
-                  <Avatar style={{ backgroundColor: '#ffd6a6' }}>Louis</Avatar>
-                  <Badge count={1}><Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" /></Badge>
-                  <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
-                  <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
-                </Card>
-              </TabPane>
-              <TabPane tab="我的团队" key="2">
-                <Teams/>
-              </TabPane>
-              <TabPane tab="我的同事" key="3">
+              <TabPane tab="我的同事" key="1">
                 <Card
                   title="智能识别同事团"
                   extra={<a href="#">添加同事</a>}
@@ -57,6 +41,22 @@ class Contacts extends PureComponent {
                     <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                     <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>KIKI</Avatar>
                     <Avatar style={{ backgroundColor: '#87d068' }} icon="car" /></p>
+                </Card>
+              </TabPane>
+              <TabPane tab="我的团队" key="2">
+                <Teams/>
+              </TabPane>
+              <TabPane tab="我的合作人" key="3">
+                <Card
+                  title="全部合作人"
+                  extra={<a href="#">添加合作人</a>}
+                  style={{ width: '95%' }}>
+                  <Avatar style={{ backgroundColor: '#4e72ab' }} icon="user" />
+                  <Avatar style={{ backgroundColor: '#c32964' }}>Terry</Avatar>
+                  <Avatar style={{ backgroundColor: '#ffd6a6' }}>Louis</Avatar>
+                  <Badge count={1}><Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" /></Badge>
+                  <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
+                  <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
                 </Card>
               </TabPane>
             </Tabs>
--- src/components/contacts/teams.js
+++ src/components/contacts/teams.js
@@ -9,7 +9,7 @@ const { userInfo } = ARGS
 const { getUrlFilterParams } = require('../../common/dataUtil')
 const { TextArea } = Input
 
-const CreateForm = Form.create()(props => {
+const TeamForm = Form.create()(props => {
   const { modalVisible, form, handleAdd, handleModalVisible } = props
   const okHandle = () => {
     form.validateFields((err, fieldsValue) => {
@@ -38,13 +38,44 @@ const CreateForm = Form.create()(props => {
   )
 })
 
+const MemberForm = Form.create()(props => {
+  const { memberModalVisible, form, handleMemberAdd, handleMemberModalVisible } = props
+  const okHandle = () => {
+    form.validateFields((err, fieldsValue) => {
+      if (err) return
+      form.resetFields()
+      handleMemberAdd(fieldsValue)
+    })
+  }
+  return (
+    <Modal
+      destroyOnClose
+      title="新建团队"
+      visible={memberModalVisible}
+      onOk={okHandle}
+      onCancel={() => handleMemberModalVisible()}>
+      <FormItem labelCol={{ span: 5 }} wrapperCol={{ span: 15 }} label="名称">
+        {form.getFieldDecorator('name', {
+          rules: [{ required: true, message: '请输入至少2个字符的团队名称！', min: 2 }],
+        })(<Input placeholder="请输入" />)}
+      </FormItem>
+      <FormItem labelCol={{ span: 5 }} key="desc" wrapperCol={{ span: 15 }} label="描述">
+        {form.getFieldDecorator('desc', {
+        })(<TextArea rows={4} placeholder="至多输入500个字符" />)}
+      </FormItem>
+    </Modal>
+  )
+})
+
 class Teams extends PureComponent {
   constructor(props) {
     super(props)
 
     this.state = {
       loading: false,
-      teamList: []
+      teamList: [],
+      teamModalVisible: false,
+      teamMemberModalVisible: false
     }
   }
 
@@ -68,16 +99,15 @@ class Teams extends PureComponent {
     })
   }
 
-
-  handleTeamModalVisible = (flag) => {
+  handleModalVisible = flag => {
     this.setState({
       teamModalVisible: !!flag,
     })
   }
 
-  handleModalVisible = flag => {
+  handleMemberModalVisible = flag => {
     this.setState({
-      teamModalVisible: !!flag,
+      teamMemberModalVisible: !!flag,
     })
   }
 
@@ -97,11 +127,29 @@ class Teams extends PureComponent {
     })
   }
 
+  handleMemberAdd = fields => {
+    let self = this
+    fields.userId = userInfo.user.userId
+    axios.post(`${ARGS.apiServer}/teams/create`, fields)
+    .then(function (response) {
+      if (response.status === 200) {
+        self.handleModalVisible()
+        message.success('添加成功', 0.5)
+        self.fetchTeamsWithUser()
+      }
+    })
+    .catch(function () {
+      message.warning('系统错误')
+    })
+  }
+
   render() {
-    const { loading, teamModalVisible, teamList } = this.state
+    const { loading, teamModalVisible, teamMemberModalVisible, teamList } = this.state
     const parentMethods = {
       handleAdd: this.handleAdd,
+      handleMemberAdd: this.handleMemberAdd,
       handleModalVisible: this.handleModalVisible,
+      handleMemberModalVisible: this.handleMemberModalVisible,
     }
     return (
       <div className="cardList">
@@ -114,7 +162,7 @@ class Teams extends PureComponent {
             item ? (
               <List.Item key={item.id}>
                 <Card hoverable className="card"
-                  actions={[<a><Icon type="plus"/> 新增成员</a>, <a>修改</a>]}>
+                  actions={[<a><Icon type="plus" onClick={() => this.handleMemberModalVisible(true)}/> 新增成员</a>, <a>修改</a>]}>
                   <Card.Meta
                     avatar={<Avatar shape="square" size="large" alt="" style={{ backgroundColor: item.color }}>{item.name.substr(0, 1)}</Avatar>}
                     title={<a>{item.name}</a>}
@@ -123,13 +171,14 @@ class Teams extends PureComponent {
               </List.Item>
             ) : (
               <List.Item>
-                <Button type="dashed" className="newButton" onClick={() => this.handleTeamModalVisible(true)}>
+                <Button type="dashed" className="newButton" onClick={() => this.handleModalVisible(true)}>
                   <Icon type="plus"/> 新增团队
                 </Button>
               </List.Item>
             )
           }/>
-        <CreateForm {...parentMethods} modalVisible={teamModalVisible} />
+        <TeamForm {...parentMethods} modalVisible={teamModalVisible} />
+        <MemberForm {...parentMethods} modalVisible={teamMemberModalVisible} />
       </div>
     )
   }
