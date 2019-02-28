18b7e72d92984489987e8ec8128c740ee4a60af9
Louis Weng <louis396@hotmail.com>
2019-02-28 16:25:42 +0800

    use yield call project upload

--- src/components/activity.js
+++ src/components/activity.js
@@ -126,7 +126,6 @@ class ActivityPane extends PureComponent {
   }
 
   render() {
-    console.log(this.style)
     return (
       <div
         className={cx({ 'activity-pane': true, 'busy': this.isBusy })}
