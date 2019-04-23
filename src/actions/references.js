3efe8225e4c08f925989ee7ac577b68729239208
Louis Weng <louis396@hotmail.com>
2019-04-23 18:27:55 +0800

    Revert: load references photos according to tag id

---
+++ src/actions/references.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const { REFERENCES } = require('../constants')
+
+module.exports = {
+
+  load(payload, meta) {
+    return {
+      type: REFERENCES.LOAD,
+      payload,
+      meta: { cmd: 'project', ...meta }
+    }
+  },
+
+  loaded(payload, meta) {
+    return {
+      type: REFERENCES.LOADED,
+      payload,
+      meta: { ...meta }
+    }
+  },
+
+  save(payload, meta) {
+    return {
+      type: REFERENCES.SAVE,
+      payload,
+      meta: { cmd: 'project', history: 'merge', ...meta }
+    }
+  },
+
+  insert(payload, meta) {
+    return {
+      type: REFERENCES.INSERT,
+      payload,
+      meta: { ...meta }
+    }
+  }
+
+}
