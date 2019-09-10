382b0a00c6ba88bd918f7b7497861e223be86fe6
Louis Weng <louis396@hotmail.com>
2019-09-10 16:13:36 +0800

    Add limitation in foot bar to forbidden uncloud photo submit

--- src/components/esper/footToolbar.js
+++ src/components/esper/footToolbar.js
@@ -3,12 +3,12 @@
 const React = require('react')
 const { PureComponent } = React
 const { Toolbar, ToolGroup, ToolbarLeft } = require('../toolbar')
-const { Button, Icon } = require('antd')
+const { Button, Icon, Tooltip } = require('antd')
 const ButtonGroup = Button.Group
 const { func, object, array } = require('prop-types')
 const { emit } = require('../../dom')
 const moment = require('moment')
-
+const __ = require('underscore')
 
 class EsperFootToolbar extends PureComponent {
 
@@ -26,10 +26,19 @@ class EsperFootToolbar extends PureComponent {
     const { photo, selections } = this.props
     let modifiedTime = ''
     if (photo) modifiedTime = moment(new Date(photo.modified)).format('MMM Do YYYY, h:mm:ss a')
+    const photoDisable = !__.isEmpty(photo) && !photo.syncPhotoId
+    const confirmButton = (<Button type="default" size="small" onClick={this.confirmPhoto} disabled={photoDisable}>
+      <Icon type="save"/>
+      提交
+    </Button>)
+    const skipButton = (<Button type="default" size="small" onClick={this.ignorePhoto}
+      disabled={photoDisable}>
+      跳过
+    </Button>)
     return (
       <Toolbar isDraggable={false}>
         <ToolbarLeft>
-          <div style={{ position: 'fixed' }}>
+          <div style={{ position: 'fixed', fontSize: '12px' }}>
             <ToolGroup>
               {(selections && selections.length > 0) ? `Date modified: ${moment(new Date(selections[selections.length - 1].updatedTime)).format('MMM Do YYYY, h:mm:ss a')}` :
               `Date modified: ${modifiedTime}`}
@@ -38,15 +47,14 @@ class EsperFootToolbar extends PureComponent {
         </ToolbarLeft>
         <div className="toolbar-center" style={{ margin: 'auto' }}>
           <ToolGroup>
-            <ButtonGroup>
-              <Button type="default" size="small" onClick={this.confirmPhoto}>
-                <Icon type="save"/>
-                提交
-              </Button>
-              <Button type="default" size="small" onClick={this.ignorePhoto}>
-                跳过
-              </Button>
-            </ButtonGroup>
+            {photoDisable ? <ButtonGroup>
+              <Tooltip placement="top" title={'请先同步上云'}>
+                {confirmButton}
+              </Tooltip>
+              <Tooltip placement="top" title={'请先同步上云'}>
+                {skipButton}
+              </Tooltip>
+            </ButtonGroup> : <ButtonGroup>{confirmButton}{skipButton}</ButtonGroup>}
           </ToolGroup>
         </div>
       </Toolbar>
