113bcaa6e261f77bdaf32eae625c6994ef97e28d
Louis Weng <louis396@hotmail.com>
2019-10-14 01:33:17 +0800

    summary localization

--- src/components/projectSummary/summary.js
+++ src/components/projectSummary/summary.js
@@ -5,9 +5,9 @@ const { PureComponent } = React
 const { Row, Col, Radio } = require('antd')
 const { Chart, Geom, Axis, Tooltip, Coord } = require('bizcharts')
 const { array, object, func } = require('prop-types')
-const { FormattedMessage } = require('react-intl')
+const { FormattedMessage, intlShape, injectIntl } = require('react-intl')
 
-class Summary extends PureComponent {
+const Summary = injectIntl(class extends PureComponent {
 
   render() {
     const cols = {
@@ -20,7 +20,7 @@ class Summary extends PureComponent {
         <Row gutter={24}>
           <Col span={12}>
             <div>
-              <div className="title"><FormattedMessage id="summary.projectProgress"/>:</div>
+              <div className="title"><FormattedMessage id="summary.projectProgress"/>: </div>
               <div className="details">
                 <div className="summary-progress"><div className="label">{taskStatuses.submitted}</div><div className="val"><FormattedMessage id="summary.submitted"/></div></div>
                 <div className="summary-progress"><div className="label">{taskStatuses.open}</div><div className="val"><FormattedMessage id="summary.remaining"/></div></div>
@@ -29,15 +29,15 @@ class Summary extends PureComponent {
               </div>
             </div>
             <div style={{ paddingTop: '24px' }}>
-              <div style={{ paddingBottom: '5px', float:'right' }}>
+              <div style={{ paddingBottom: '5px', float: 'right' }}>
                 <Radio.Group defaultValue="HH" size="small" onChange={this.props.switchActivityData}>
-                  <Radio.Button value="MM">Last Hour</Radio.Button>
-                  <Radio.Button value="HH">Today</Radio.Button>
-                  <Radio.Button value="DD">Last 7 days</Radio.Button>
+                  <Radio.Button value="MM"><FormattedMessage id="summary.activity.lastHour"/></Radio.Button>
+                  <Radio.Button value="HH"><FormattedMessage id="summary.activity.today"/></Radio.Button>
+                  <Radio.Button value="DD"><FormattedMessage id="summary.activity.last7days"/></Radio.Button>
                 </Radio.Group>
               </div>
-              <div className="title">Labeling Activity:</div>
-              <Chart height={400} data={activityData} scale={cols} forceFit placeholder="暂无数据">
+              <div className="title"><FormattedMessage id="summary.activity.title"/>:</div>
+              <Chart height={400} data={activityData} scale={cols} forceFit placeholder={this.props.intl.formatMessage({ id: 'common.noData' })}>
                 <Axis name="Time" />
                 <Axis name="SumCount" />
                 <Tooltip crosshairs={{ type: 'y' }}/>
@@ -48,8 +48,8 @@ class Summary extends PureComponent {
           </Col>
           <Col span={12}>
             <div>
-              <div className="title">Skus:</div>
-              <Chart height={300} data={skuData} forceFit placeholder="暂无数据">
+              <div className="title"><FormattedMessage id="summary.targets"/>: </div>
+              <Chart height={300} data={skuData} forceFit placeholder={this.props.intl.formatMessage({ id: 'common.noData' })}>
                 <Coord transpose />
                 <Axis name="name" label={{ offset: 12 }} />
                 <Axis name="count" />
@@ -60,8 +60,8 @@ class Summary extends PureComponent {
               </Chart>
             </div>
             <div>
-              <div className="title">任务完成率:</div>
-              <Chart height={300} data={userPhotoStatusData} forceFit placeholder="暂无数据">
+              <div className="title"><FormattedMessage id="summary.taskCompletionRate"/>: </div>
+              <Chart height={300} data={userPhotoStatusData} forceFit placeholder={this.props.intl.formatMessage({ id: 'common.noData' })}>
                 <Coord transpose />
                 <Axis name="name" label={{ offset: 12 }} />
                 <Axis name="percentage" />
@@ -75,12 +75,13 @@ class Summary extends PureComponent {
     )
   }
   static propTypes = {
+    intl: intlShape.isRequired,
     skuData: array.isRequired,
     taskStatuses: object.isRequired,
     activityData: array.isRequired,
     switchActivityData: func.isRequired,
     userPhotoStatusData: array.isRequired
   }
-}
+})
 
 module.exports = { Summary }
