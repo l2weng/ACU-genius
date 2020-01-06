15e56dab3e337ef3fa3d43065bc7e5b34f2db6ad
Louis Weng <louis396@hotmail.com>
2020-01-06 10:30:39 +0800

    update project cache way

--- src/browser/labelReal.js
+++ src/browser/labelReal.js
@@ -66,8 +66,8 @@ class LabelReal extends EventEmitter {
     webgl: true,
     win: {},
     userInfo: {},
-    // apiServer: 'http://101.132.45.191:3000/lr',
-    apiServer: 'http://127.0.0.1:3000/lr',
+    apiServer: 'http://101.132.45.191:3000/lr',
+    // apiServer: 'http://127.0.0.1:3000/lr',
     projectsCache: {},
     zoom: 1.0
   }
